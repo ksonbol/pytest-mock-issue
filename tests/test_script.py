9db41fbf850f890f104bd6fb300ab38bc11c8dd1
Karim Sonbol <karim.sonbol@adevinta.com>
2022-05-29 16:57:20 +0200

    use patch instead of magic mock

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,16 +1,14 @@
 import os
-from unittest.mock import MagicMock
+from unittest.mock import patch
 
-import src
 from src import script
 
 
 def test_build_writer(tmp_path):
-    mock_writer = MagicMock()
-    src.script.tf.io.TFRecordWriter = mock_writer
-    filename = "test.tfrecord"
-    src.script.build_writer(os.path.join(tmp_path, filename))
-    mock_writer.assert_called_with(os.path.join(tmp_path, filename))
+    with patch("src.script.tf.io.TFRecordWriter") as mock_writer:
+        filename = "test.tfrecord"
+        script.build_writer(os.path.join(tmp_path, filename))
+        mock_writer.assert_called_with(os.path.join(tmp_path, filename))
 
 
 def test_use_writer(tmp_path):
